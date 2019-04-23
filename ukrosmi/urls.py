0ffeb949ec7f30a5b9a774b26a5092a8f40797a7
docker <docker@MacBook-Air.local>
2019-04-23 11:24:56 +0300

    добавил прочтименя

--- ukrosmi/urls.py
+++ ukrosmi/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls.static import static
+from gnews import views as gnews_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', gnews_views.index),
 ]
